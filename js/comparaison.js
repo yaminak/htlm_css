48dda5667f847aeb7fbdc9a9453048648612a54e
yaminakara <yamina.karamane@lepoles.org>
2022-04-29 16:47:19 +0200

    suite15

---
+++ js/comparaison.js
@@ -0,0 +1,13 @@
+// var calcul = 15;
+// console.log(calcul);
+
+// var total = "mila";
+// var portable = total + "<br>" + calcul;
+// document.write(portable);
+// var phone = false;
+// var euro = prompt("quel montant?");
+// var total = euro * 1.06;
+// document.write(total);
+// prompt();
+// var doc = document.write("hier");
+// console.log(doc);
