9a2b98f8466ecf3162a62e1b2a308385e312194a
yaminakara <yamina.karamane@lepoles.org>
2022-05-03 16:56:50 +0200

    suite17

--- js/exo2_5_22.js
+++ js/exo2_5_22.js
@@ -16,21 +16,50 @@
 // document.write(typeof monChiffre);
 // var monChiffreDecimal = 3.14;
 // document.write(typeof monChiffreDecimal);
-var maChaineDeCaractere = "57.12";
-console.log(typeof maChaineDeCaractere);
-var maChaineDeCaractere2 = parseFloat(maChaineDeCaractere);
-console.log(typeof maChaineDeCaractere);
-var maChaineDeCaractere3 = parseInt(maChaineDeCaractere2);
-console.log(maChaineDeCaractere3);
-console.log(typeof maChaineDeCaractere3);
-var maChaineDeCaractere4 = maChaineDeCaractere3.toString();
-console.log(typeof maChaineDeCaractere4);
-document.write(
-  maChaineDeCaractere +
-    "-" +
-    maChaineDeCaractere2 +
-    "-" +
-    maChaineDeCaractere3 +
-    "-" +
-    maChaineDeCaractere4
-);
+// var maChaineDeCaractere = "57.12";
+// console.log(typeof maChaineDeCaractere);
+// var maChaineDeCaractere2 = parseFloat(maChaineDeCaractere);
+// console.log(typeof maChaineDeCaractere);
+// var maChaineDeCaractere3 = parseInt(maChaineDeCaractere2);
+// console.log(maChaineDeCaractere3);
+// console.log(typeof maChaineDeCaractere3);
+// var maChaineDeCaractere4 = maChaineDeCaractere3.toString();
+// console.log(typeof maChaineDeCaractere4);
+// document.write(
+//   maChaineDeCaractere +
+//     "-" +
+//     maChaineDeCaractere2 +
+//     "-" +
+//     maChaineDeCaractere3 +
+//     "-" +
+//     maChaineDeCaractere4
+// );
+
+// var annee = 2017;
+// var futur = 3;
+// var calcul = annee + futur;
+// console.log(calcul);
+// document.write(calcul + "&nbsp&nbsp" + calcul + "<br>");
+// document.write(calcul + "<br>");
+
+var nb1, nb2, resultat;
+nb1 = 10;
+nb2 = 5;
+resultat = nb1 + nb2;
+console.log(resultat);
+document.write(resultat);
+var resultatSoustraction;
+resultatSoustraction = nb1 - nb2;
+console.log(resultatSoustraction);
+document.write(resultatSoustraction);
+var resultatMultiplicaction;
+resultatMultiplicaction = nb1 * nb2;
+console.log(resultatMultiplicaction);
+document.write(resultatMultiplicaction);
+var resultatDivision;
+resultatDivision = nb1 / nb2;
+console.log(resultatDivision);
+document.write(resultatDivision);
+var modulo = nb1 % nb2;
+console.log(modulo);
+document.write(modulo);
