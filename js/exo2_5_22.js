b5097e5eb960cc1bb42077ef08bbd8228e14ecf3
yaminakara <yamina.karamane@lepoles.org>
2022-05-04 16:57:05 +0200

    suite18

--- js/exo2_5_22.js
+++ js/exo2_5_22.js
@@ -42,24 +42,72 @@
 // document.write(calcul + "&nbsp&nbsp" + calcul + "<br>");
 // document.write(calcul + "<br>");
 
-var nb1, nb2, resultat;
-nb1 = 10;
-nb2 = 5;
-resultat = nb1 + nb2;
-console.log(resultat);
-document.write(resultat);
-var resultatSoustraction;
-resultatSoustraction = nb1 - nb2;
-console.log(resultatSoustraction);
-document.write(resultatSoustraction);
-var resultatMultiplicaction;
-resultatMultiplicaction = nb1 * nb2;
-console.log(resultatMultiplicaction);
-document.write(resultatMultiplicaction);
-var resultatDivision;
-resultatDivision = nb1 / nb2;
-console.log(resultatDivision);
-document.write(resultatDivision);
-var modulo = nb1 % nb2;
-console.log(modulo);
-document.write(modulo);
+// var nb1, nb2, resultat;
+// nb1 = 10;
+// nb2 = 5;
+// resultat = nb1 + nb2;
+// console.log(resultat);
+// document.write(resultat);
+// var resultatSoustraction;
+// resultatSoustraction = nb1 - nb2;
+// console.log(resultatSoustraction);
+// document.write(resultatSoustraction);
+// var resultatMultiplicaction;
+// resultatMultiplicaction = nb1 * nb2;
+// console.log(resultatMultiplicaction);
+// document.write(resultatMultiplicaction);
+// var resultatDivision;
+// resultatDivision = nb1 / nb2;
+// console.log(resultatDivision);
+// document.write(resultatDivision);
+// var modulo = nb1 % nb2;
+// console.log(modulo);
+// document.write(modulo);
+
+// alert("je sais!");
+// var jour;
+// jour = prompt("quel jour de la semaine", "lundi");
+// document.write(jour);
+
+// var nb1, nb2, nb3, nb4;
+// nb1 = "mercredi";
+// nb2 = 4;
+// nb3 = "mai";
+// nb4 = 2022;
+// console.log(nb1 + "" + nb2 + "-" + nb3 + "-" + nb4);
+// document.write(nb1 + "&nbsp" + nb2 + "&nbsp" + nb3 + "&nbsp" + nb4);
+
+// nb1 = parseInt(nb1);
+// console.log(typeof nb1);
+
+// var prix;
+// prix = prompt("quel montant?");
+// var tva = 1.02;
+// var resultat = prix * tva;
+// console.log(resultat);
+// document.write(resultat);
+
+// var nombreEntier = prompt("Entrez un nombre entier");
+
+// if (nombreEntier > 10) {
+//   document.write("yamina");
+// }
+
+// var prenom = prompt("entrez votre prénom");
+// if (prenom == "yamina") {
+//   document.write(prenom);
+// } else {
+//   document.write("cette valeur n'est pas mon prénom");
+// }
+
+// var nombre = prompt("votre nombre");
+// var resultat = nombre % 5;
+
+// if (resultat == 0) {
+//   document.write(nombre);
+// } else {
+//   document.write("ce n'est pas un multiple de 5");
+// }
+
+var nb2 = Number(prompt("entrez nb1 :"));
+document.write(typeof nb2);
